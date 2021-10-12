e6ebd306854e4da38bac9d068f44776f45707efc
Murad Marko Prodanovic <muradmarkoprodanovic@MacBook-Air-von-Murad.local>
2021-10-12 18:11:43 +0200

    Changed

---
+++ src/main/java/com/indizon/indizon/IndizonApplication.java
@@ -0,0 +1,13 @@
+package com.indizon.indizon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndizonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndizonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/indizon/indizon/IndizonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.indizon.indizon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndizonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
