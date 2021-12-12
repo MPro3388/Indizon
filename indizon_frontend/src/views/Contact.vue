9e95f23add2ede0d3a3a4f4109dc411df6deff09
markoprodanovic <s0543834@htw-berlin.de>
2021-12-12 12:27:28 +0100

    Vue Frontend

---
+++ indizon_frontend/src/views/Contact.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="home">
+    <img class="img" alt="Blue Shield logo" src="../assets/BlueShield.png">
+    <HelloWorld msg="Contact Formular"/>
+
+    <form id="contact">
+      <label id="h2" form="contact">Kontaktformular</label>
+      <fieldset>
+        <legend>Name</legend>
+
+        <label for="vorname">
+          Vorname: <input name="vorname" id="vorname" placeholder="Max">
+        </label>
+
+        <label for="nachname">
+          Nachname: <input name="nachname" id="nachname" placeholder="Mustermann">
+        </label>
+
+      </fieldset>
+
+      <fieldset>
+        <legend>Geburtsdatum</legend>
+
+        <label for="birth">
+           <input name="birth" id="birth" placeholder="TT.MM.JJJJ">
+        </label>
+
+      </fieldset>
+
+      <fieldset>
+        <legend>Geschlecht</legend>
+
+        <label for="männl">
+          <input type="radio" name="geschlecht" id="männl" value="männl">
+          männlich
+        </label>
+
+        <label for="weibl">
+          <input type="radio" name="geschlecht" id="weibl" value="weibl">
+          weiblich
+        </label>
+
+      </fieldset>
+
+      <fieldset>
+        <legend>Anliegen</legend>
+
+       <h1>Anliegen</h1>
+        <p>Schreiben Sie ihr Anliegen:</p>
+        <textarea cols="50" rows="10"></textarea>
+
+      </fieldset>
+
+      <button type="reset">Eingaben zurücksetzen</button>
+      <button type="submit">Eingaben absenden</button>
+    </form>
+
+  </div>
+</template>
+
+<style>
+.img{
+  width: 350px;
+  height: 300px
+}
+
+#contact{
+  text-align: center;
+  width: 50%;
+  height: 100%;
+  margin-left: auto;
+  margin-right: auto;
+  border: 0;
+  cellpadding:0;
+  cellspacing: 0;
+}
+
+#h2{
+  font-size: 40px;
+  font-style: inherit;
+  font-palette: dark;
+}
+</style>
---
+++ indizon_frontend/src/views/Home.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="home">
+    <img class="img" alt="Indizon logo" src="../assets/BlueShield.png">
+    <HelloWorld msg="Welcom to Indizon Shop"/>
+    <table class="Tabelle">
+      <tr>
+        <th>Product</th>
+        <th>Preis</th>
+      </tr>
+      <tr>
+        <td>
+          Haarcreme
+        </td>
+
+        <td>2,99</td>
+
+      </tr>
+      <tr>
+        <td>
+          USB-Stick 32 GB
+        </td>
+        <td>14,99</td>
+      </tr>
+      <tr>
+        <td>
+          Hugo Boxershorts 3erPack
+        </td>
+        <td>19,99</td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+<style>
+.img{
+  width: 350px;
+  height: 300px
+}
+
+.Tabelle{
+  text-align: center;
+  width: 30%;
+  height: 100%;
+  margin-left: auto;
+  margin-right: auto;
+  border: 0;
+  cellpadding:0;
+  cellspacing: 0;
+}
+</style>
